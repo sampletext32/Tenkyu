bb398592893993f19232d9a05c095fb2aea19599
sampletext32 <sampletext32@bk.ru>
2020-07-25 12:51:56 +0300

    Make world rotation

--- Assets/Scripts/InputTilt.cs
+++ Assets/Scripts/InputTilt.cs
@@ -8,7 +8,7 @@ using UnityEngine.UIElements;
 public class InputTilt : MonoBehaviour
 {
     [SerializeField]
-    private Rigidbody _target;
+    private Transform _target;
 
     private Transform _selfTransform;
 
@@ -24,39 +24,45 @@ public class InputTilt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputPosition;
+        Vector2 mousePosition;
 
         if (Application.platform == RuntimePlatform.Android && Input.touches.Length > 0)
         {
-            inputPosition = Input.touches[0].position;
+            mousePosition = Input.touches[0].position;
         }
         else if (Input.GetMouseButton((int)MouseButton.LeftMouse))
         {
-            inputPosition = Input.mousePosition;
+            mousePosition = Input.mousePosition;
         }
         else
         {
             return;
         }
 
-
-        var inputPositionX = ((Screen.width - inputPosition.x) / Screen.width - 0.5f) * 2f;
-        var inputPositionY = ((Screen.height - inputPosition.y) / Screen.height - 0.5f) * 2f;
-
         // y converts into around z tilt
         // x converts into around x tilt
 
+        Vector3 inputTiltAngle = new Vector3(
+            ((Screen.width - mousePosition.x) / Screen.width - 0.5f) * 2f,
+            0,
+            ((Screen.height - mousePosition.y) / Screen.height - 0.5f) * 2f
+        );
 
         // Debug.Log($"{inputPositionX}:{inputPositionY}");
 
-        //_target.AddForce(inputPositionX, 0, inputPositionY);
-        //_target.rotation = Quaternion.Euler(inputPositionX, 0, inputPositionY);
+        // _target.AddForce(inputPositionX, 0, inputPositionY);
 
-        inputPositionX *= 180 / Mathf.PI;
-        inputPositionY *= 180 / Mathf.PI;
+        // -90 to 90 degrees
+        inputTiltAngle *= 180 / Mathf.PI;
 
-        _follow._offsetDirection.y = -inputPositionY;
-        _follow._offsetDirection = _follow._offsetDirection.normalized;
+        // -18 to 18 degrees
+        inputTiltAngle *= 1 / 5f;
+
+        _target.rotation = Quaternion.Lerp(_target.rotation, Quaternion.Euler(inputTiltAngle), 0.9f);
+
+        // 
+        // _follow._offsetDirection.y = -inputPositionY;
+        // _follow._offsetDirection = _follow._offsetDirection.normalized;
 
         // _selfTransform.rotation =
         //     Quaternion.Euler(_selfTransform.rotation.x - inputPositionY, _selfTransform.rotation.y,
