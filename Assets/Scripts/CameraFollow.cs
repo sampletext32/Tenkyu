19acc5bedb4f2632dfb202240b8097015ea9654b
sampletext32 <sampletext32@bk.ru>
2020-07-25 13:20:06 +0300

    Strong easing

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] 
+    [SerializeField]
     Transform _target;
 
     [SerializeField]
@@ -14,19 +14,29 @@ public class CameraFollow : MonoBehaviour
     [SerializeField]
     public Vector3 _offsetDirection;
 
+    [SerializeField]
+    [Range(0, 1)]
+    public float _easing;
+
     private Transform _selfTransform;
+
+    private Vector3 _lookAt;
 
     // Start is called before the first frame update
     void Start()
     {
         _selfTransform = transform;
+        _lookAt = _target.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _selfTransform.position = Vector3.Lerp(_selfTransform.position, _target.position + _offsetDirection * _distance, 0.8f);
-        _selfTransform.LookAt(_target);
+        _selfTransform.position =
+            Vector3.Lerp(_selfTransform.position, _lookAt + _offsetDirection * _distance, _easing);
+        _lookAt = Vector3.Lerp(_lookAt, _target.position, _easing);
+        _selfTransform.rotation = Quaternion.Lerp(_selfTransform.rotation,
+            Quaternion.LookRotation(_lookAt - _selfTransform.position, Vector3.up), _easing);
         // Destroy(this);
     }
 }
